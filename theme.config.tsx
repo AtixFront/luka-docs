5322e8c815f1cbfb33b6dc600dde8f944ace51d9
ChrisVelasco0312 <cristianvelasco0312@gmail.com>
2023-06-15 23:42:38 -0500

    [feature] Base template with nextra

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Luka Docs</span>,
+  project: {
+    link: 'https://github.com/AtixFront/luka-docs.git',
+  },
+  chat: {
+    link: 'https://discord.com',
+  },
+  docsRepositoryBase: 'https://github.com/AtixFront/luka-docs.git',
+  footer: {
+    text: 'Luka Docs Wallet App',
+  },
+}
+
+export default config
